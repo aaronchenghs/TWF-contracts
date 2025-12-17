567d585f39d1b71407af88dcd273fe1678f81b62
Aaron Cheng <acheng8@lsu.edu>
2025-12-16 22:05:13 -0600

    update dist

--- dist/guid.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Nominal/Branded string type for UUID/GUID values.
- * Prevents accidentally passing plain strings where a Guid is expected.
- */
-export declare type Guid = string & {
-    readonly __brand: "Guid";
-};
-export declare function newGuid(): Guid;
-export declare function isGuid(value: string): value is Guid;
