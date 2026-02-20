70778eccbe20ddab2bec340e2957a58351438cbd
Aaron Cheng <acheng8@lsu.edu>
2026-02-19 21:34:28 -0600

    upgrade avatar parts count

--- src/codes.ts
+++ src/codes.ts
@@ -5,5 +5,5 @@ export const LOBBY_CAPACITY = 8;
 export const AVATAR_BASE = 36;
 export const AVATAR_PARTS_COUNT = 3;
 export const AVATAR_BODY_COUNT = 18;
-export const AVATAR_MOUTH_COUNT = 9;
-export const AVATAR_EYES_COUNT = 8;
+export const AVATAR_MOUTH_COUNT = 10;
+export const AVATAR_EYES_COUNT = 9;
